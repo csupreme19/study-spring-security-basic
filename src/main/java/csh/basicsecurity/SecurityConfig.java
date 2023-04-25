ba24817baea940298266a40ba8b79c82691b01d6
csupreme19 <csupreme19@gmail.com>
2023-04-25 20:00:25 +0900

    remember me 사용

--- src/main/java/csh/basicsecurity/SecurityConfig.java
+++ src/main/java/csh/basicsecurity/SecurityConfig.java
@@ -1,16 +1,21 @@
 package csh.basicsecurity;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,6 +36,11 @@ public class SecurityConfig {
                 })
                 .deleteCookies("remember-me")
         ;
+
+        http.rememberMe()
+                .rememberMeParameter("remember-me")
+                .tokenValiditySeconds(60 * 60 * 24 * 14)
+                .userDetailsService(userDetailsService);
 
         return http.build();
     }
