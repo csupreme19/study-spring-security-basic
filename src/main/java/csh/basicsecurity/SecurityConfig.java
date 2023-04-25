e8b9ae8a992ca4f46c1843c07f3ff7e4220e4c09
csupreme19 <csupreme19@gmail.com>
2023-04-25 20:42:13 +0900

    익명 사용자 필터 확인

--- src/main/java/csh/basicsecurity/SecurityConfig.java
+++ src/main/java/csh/basicsecurity/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .anyRequest().authenticated();
+                .anyRequest().anonymous();
 
         http.formLogin();
 
