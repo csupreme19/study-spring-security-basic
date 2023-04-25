d111728197e8a91c40eb318aec7fd49bb5f1776b
csupreme19 <csupreme19@gmail.com>
2023-04-25 18:40:23 +0900

    formLogin 관련 기능 확인

--- src/main/java/csh/basicsecurity/SecurityConfig.java
+++ src/main/java/csh/basicsecurity/SecurityConfig.java
@@ -1,11 +1,13 @@
 package csh.basicsecurity;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -14,7 +16,25 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .anyRequest().authenticated();
-        http.formLogin();
+
+        http.formLogin()
+//                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/loginPage")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler((req, res, auth) -> {
+                    log.info("authentication success={}", auth.getName());
+                    res.sendRedirect("/");
+                })
+                .failureHandler((req, res, exp) -> {
+                    log.info("authentication failed={}", exp.getMessage());
+                    res.sendRedirect("/loginPage");
+                })
+                .permitAll()
+        ;
+
         return http.build();
     }
 
